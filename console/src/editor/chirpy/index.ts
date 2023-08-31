1e7e4ddc94557869035ce325853cb1db6f1047d7
AirboZH <i@airbozh.cn>
2023-08-31 17:17:44 +0800

    feat: 完成ChirpyPrompt

---
+++ console/src/editor/chirpy/index.ts
@@ -0,0 +1,77 @@
+import {mergeAttributes, Node, textblockTypeInputRule, wrappingInputRule} from "@tiptap/core";
+
+export interface ChirpyPromptOptions {
+  types: string[],
+  HTMLAttributes: Record<string, any>,
+}
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    prompt: {
+      /**
+       * Set a Prompt setType
+       */
+      setType: (attributes: { type: string }) => ReturnType,
+    }
+  }
+}
+
+export const ChirpyPrompt = Node.create<ChirpyPromptOptions>({
+  name: "chirpyPrompt",
+
+  addOptions() {
+    return {
+      types: ['tip', 'info', 'warning', 'danger'],
+      HTMLAttributes: {},
+    }
+  },
+
+  content: 'block+',
+
+  group: 'block',
+
+  defining: true,
+
+  addAttributes() {
+    return {
+      type: {
+        default: 'tip',
+      }
+    }
+  },
+
+  parseHTML() {
+    return this.options.types
+      .map(type => ({
+        tag: `blockquote[class="prompt-${type}"]`,
+        attrs: {type},
+      }))
+  },
+
+  renderHTML({node, HTMLAttributes}) {
+    const hasType = this.options.types.includes(node.attrs.type)
+    const type = hasType ? node.attrs.type : this.options.types[0]
+    return ['blockquote', mergeAttributes(HTMLAttributes, {class: `prompt-${type}`,}), 0]
+  },
+
+  addCommands() {
+    return {
+      setType: attributes => ({commands}) => {
+        if (!this.options.types.includes(attributes.type)) {
+          return false
+        }
+        return commands.setNode(this.name, attributes)
+      },
+    }
+  },
+
+  addInputRules() {
+    return this.options.types.map(type => {
+      return wrappingInputRule({
+        find: new RegExp(`^(:::|:::${type})\\s$`),
+        type: this.type,
+        getAttributes: {type},
+      })
+    })
+  },
+})
--- console/src/index.ts
+++ console/src/index.ts
@@ -1,29 +1,10 @@
 import { definePlugin } from "@halo-dev/console-shared";
-import HomeView from "./views/HomeView.vue";
-import { IconPlug } from "@halo-dev/components";
-import { markRaw } from "vue";
+import {ChirpyPrompt} from "@/editor/chirpy";
 
 export default definePlugin({
-  components: {},
-  routes: [
-    {
-      parentName: "Root",
-      route: {
-        path: "/example",
-        name: "Example",
-        component: HomeView,
-        meta: {
-          title: "示例页面",
-          searchable: true,
-          menu: {
-            name: "示例页面",
-            group: "示例分组",
-            icon: markRaw(IconPlug),
-            priority: 0,
-          },
-        },
-      },
-    },
-  ],
-  extensionPoints: {},
+  extensionPoints: {
+    "default:editor:extension:create": () => {
+      return [ChirpyPrompt]
+    }
+  },
 });
